f61aed61fe5262d254429f64a36272b7b5b46583
Medas <medas02345@gmail.com>
2022-07-04 14:47:10 +0300

    Modify function createGrid, for it to delete the current grid if creating another one

--- javascript.js
+++ javascript.js
@@ -1,12 +1,20 @@
 
 function createGrid(){
-    let container = document.getElementById("container");
-    for(let i = 0; i < 100; i++){
+    if(document.querySelector("#container") != null){
+        const currentGrid = document.querySelector("#container");
+        document.body.removeChild(currentGrid);
+    }
+    let gridSize = document.getElementById("gridSize").value;
+    console.log(gridSize);
+    const container = document.createElement("table");
+    container.id = "container";
+    document.body.appendChild(container);
+    for(let i = 0; i < gridSize; i++){
         let gridRow = document.createElement("tr");
         gridRow.id = "row" + i;
         container.appendChild(gridRow);
         let rowW = document.getElementById("row" + i);
-        for(let j = 0; j < 100; j++){
+        for(let j = 0; j < gridSize; j++){
             let gridCell = document.createElement("td");
             rowW.appendChild(gridCell);
             gridCell.className = "cell";
@@ -16,13 +24,7 @@ function createGrid(){
         }
 
         
-        
     }
 }
 createGrid();
-/** 
-gridCell.addEventListener("mouseover", mouseOver, false);
-        function mouseOver(){
-            gridCell.setAttribute('style','background-color:black');
-        }
-*/
+
