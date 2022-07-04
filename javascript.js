f51f5c00d06afc3b9657e593ada82146abd5eb5f
Medas <medas02345@gmail.com>
2022-07-04 20:35:50 +0300

    Add styling, include color chooser color as a paint color

--- javascript.js
+++ javascript.js
@@ -1,45 +1,43 @@
-function randomRgbColor() {
-    let r = randomInteger(255);
-    let g = randomInteger(255);
-    let b = randomInteger(255);
-    return [r,g,b];
-}
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-  }
+function createGrid() {
 
-function createGrid(){
-    if(document.querySelector("#container") !== null){
+    if (document.querySelector("#container") !== null) {
         const currentGrid = document.querySelector("#container");
         document.body.removeChild(currentGrid);
     }
+
     let gridSize = document.getElementById("gridSize").value;
     const container = document.createElement("table");
     container.id = "container";
     document.body.appendChild(container);
-    for(let i = 0; i < gridSize; i++){
+
+    for (let i = 0; i < gridSize; i++) {
+
         let gridRow = document.createElement("tr");
         gridRow.id = "row" + i;
         container.appendChild(gridRow);
+
+
         let rowW = document.getElementById("row" + i);
-        for(let j = 0; j < gridSize; j++){
+        for (let j = 0; j < gridSize; j++) {
             let gridCell = document.createElement("td");
             rowW.appendChild(gridCell);
             gridCell.className = "cell";
-            gridCell.addEventListener("mouseover", ()=>{
+
+            gridCell.addEventListener("mouseover", () => {
                 const rgbButton = document.querySelector("#rgb");
-                if(rgbButton.checked){
-                    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+                if (rgbButton.checked) {
+                    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
                     gridCell.style.backgroundColor = "#" + randomColor;
                 }
-                else{
-                    gridCell.style.backgroundColor = "black";
+                else {
+                    gridCell.style.backgroundColor = document.getElementById("colorPicker").value;
+                    
                 }
-                
+
             });
         }
 
-        
+
     }
 }
 createGrid();
