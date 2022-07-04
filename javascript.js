e7b34d0245968e7aa2f1c4304f56d930ce4f6fcc
Medas <medas02345@gmail.com>
2022-07-04 14:12:19 +0300

    Add a hover effect to change cell's background color

--- javascript.js
+++ javascript.js
@@ -1,15 +1,18 @@
+
 function createGrid(){
     let container = document.getElementById("container");
-    for(let i = 0; i < 16; i++){
+    for(let i = 0; i < 100; i++){
         let gridRow = document.createElement("tr");
         gridRow.id = "row" + i;
         container.appendChild(gridRow);
         let rowW = document.getElementById("row" + i);
-        for(let j = 0; j < 16; j++){
+        for(let j = 0; j < 100; j++){
             let gridCell = document.createElement("td");
             rowW.appendChild(gridCell);
             gridCell.className = "cell";
-            
+            gridCell.addEventListener("mouseover", ()=>{
+                gridCell.setAttribute('style','background-color:black');
+            });
         }
 
         
